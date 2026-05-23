4b9f27051f5b6c54500417cc13bcfc201711ade7
kapilgarg9358 <kapilgarg9358@gmail.com>
2026-05-23 19:42:00 +0530

    feat: add ATS resume analysis application with Puter AI integration"

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   isRouteErrorResponse,
   Links,
@@ -9,6 +11,9 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import Navbar from "~/components/Navbar";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,6 +29,12 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { init } = usePuterStore();
+
+  useEffect(() => {
+    init()
+  }, [init]);
+
   return (
     <html lang="en">
       <head>
@@ -33,7 +44,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <script src="https://js.puter.com/v2/"></script>
+        <Navbar />
+        <div className="min-h-screen">
+          {children}
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
